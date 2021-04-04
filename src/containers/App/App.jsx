162e8e04b4813afdd31cb96beda9a393e8132f62
lytves <22356770+lytves@users.noreply.github.com>
2021-04-04 22:05:46 +0200

    implemented initial Routing

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,12 +1,24 @@
+import {BrowserRouter, NavLink, Route} from 'react-router-dom'
+
 import CharactersPage from '@containers/CharactersPage'
+import HomePage from '@containers/HomePage'
 
 import styles from './App.module.css'
 import cn from 'classnames'
 
 const App = () => {
-  return (
-      <CharactersPage />
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <NavLink to="/" exact>Home</NavLink>
+                <NavLink to="/characters" exact>Characters</NavLink>
+
+                <Route path="/" exact component={HomePage}/>
+                <Route path="/characters" exact component={CharactersPage}/>
+
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
