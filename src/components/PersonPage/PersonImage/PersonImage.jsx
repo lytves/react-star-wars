80d0fce030d6b8054d613a6c0707162c20d77968
lytves <22356770+lytves@users.noreply.github.com>
2021-12-06 11:56:11 +0100

    toggleFavorite icon added

--- src/components/PersonPage/PersonImage/PersonImage.jsx
+++ src/components/PersonPage/PersonImage/PersonImage.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types'
 import {useDispatch} from "react-redux";
 import {setCharacterToFavorite, removeCharacterFromFavorite} from '@store/actions'
+import iconFavorite from './img/favorite.svg'
+import iconFavoriteFilled from './img/favorite-filled.svg'
 import styles from './PersonImage.module.css'
 
 const PersonImage = ({personImage, personName, personId, personFavorite, setPersonFavorite}) => {
     const dispatch = useDispatch();
+
     const toggleFavoritePerson = () => {
         if (personFavorite) {
             dispatch(removeCharacterFromFavorite(personId));
@@ -23,8 +26,13 @@ const PersonImage = ({personImage, personName, personId, personFavorite, setPers
         <>
             <div className={styles.container}>
                 <img className={styles.image} src={personImage} alt={personName}/>
+                <img
+                    src={personFavorite ? iconFavoriteFilled : iconFavorite}
+                    alt="Add to Favorite"
+                    className={styles.favorite}
+                    onClick={toggleFavoritePerson}
+                />
             </div>
-            <button onClick={toggleFavoritePerson}>button</button>
         </>
     )
 }
