0750c08612025cead8c9b067f841d3813fbb949b
lytves <22356770+lytves@users.noreply.github.com>
2021-12-06 15:21:21 +0100

    Favorites page finished

--- src/containers/FavoritesPage/FavoritesPage.jsx
+++ src/containers/FavoritesPage/FavoritesPage.jsx
@@ -1,13 +1,37 @@
 import {useSelector} from 'react-redux'
+import {useEffect, useState} from "react";
+import CharactersList from "@components/CharactersPage/CharactersList";
 
 import styles from './FavoritesPage.module.css'
-import favoritesReducer from "@store/reducers/favoritesReducer";
 
 const FavoritesPage = ({}) => {
+    const [characters, setCharacters] = useState([])
     const storeData = useSelector(state => state.favoritesReducer);
+
+    useEffect(() => {
+        const charactersArray = Object.entries(storeData)
+        if (charactersArray.length) {
+            const res = charactersArray.map(
+                item => {
+                    return {
+                        id: item[0],
+                        ...item[1]
+                        // name: item[1].name,
+                        // img: item[1].img
+                    }
+                }
+            )
+            setCharacters(res)
+        }
+    }, [])
+
     return (
         <>
-            <h1>FavPage</h1>
+            <h1 className="header__text">Bookmarks</h1>
+            {characters.length
+                ? <CharactersList characters={characters}></CharactersList>
+                : <h2 className={styles.comment}>Add you favorites characters to your bookmarks list</h2>
+            }
         </>
     )
 }
